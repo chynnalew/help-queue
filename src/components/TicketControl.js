ef002b99b47eceae4e92a7d534d3b13951adfc51
chynnalew <chynnalew@yahoo.com>
2021-12-01 13:57:50 -0800

    add moment js

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -5,6 +5,7 @@ import TicketDetail from './TicketDetail';
 import EditTicketForm from './EditTicketForm';
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
+import * as a from './../actions';
 
 class TicketControl extends React.Component {
 
@@ -18,20 +19,31 @@ class TicketControl extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    60000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime = () => {
+    const { dispatch } = this.props;
+    Object.values(this.props.mainTicketList).forEach(ticket => {
+      const newFormattedWaitTime = ticket.timeOpen.fromNow(true);
+      const action = a.updateTime(ticket.id, newFormattedWaitTime);
+      dispatch(action);
+    });
+  }
+  
   handleAddingNewTicketToList = (newTicket) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = newTicket;
-    const action = {
-      type: 'ADD_TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue,
-    }
+    const action = a.addTicket(newTicket);
     dispatch(action);
-    const action2 = {
-      type: 'TOGGLE_FORM'
-    }
+    const action2 = a.toggleForm();
     dispatch(action2);
   }
 
@@ -43,9 +55,7 @@ class TicketControl extends React.Component {
       });
     } else {
       const { dispatch } = this.props;
-      const action = {
-        type: 'TOGGLE_FORM'
-      }
+      const action = a.toggleForm();
       dispatch(action);
     }
   }
@@ -55,15 +65,12 @@ class TicketControl extends React.Component {
     this.setState({selectedTicket: selectedTicket});
   }
 
-    handleDeletingTicket = (id) => {
-      const { dispatch } = this.props;
-      const action = {
-        type: 'DELETE_TICKET',
-        id: id
-      }
-      dispatch(action);
-      this.setState({selectedTicket: null});
-    }
+  handleDeletingTicket = (id) => {
+    const { dispatch } = this.props;
+    const action = a.deleteTicket(id);
+    dispatch(action);
+    this.setState({selectedTicket: null});
+  }
 
     handleEditClick = () => {
       this.setState({editing: true});
@@ -71,14 +78,7 @@ class TicketControl extends React.Component {
 
     handleEditingTicketInList = (ticketToEdit) => {
       const { dispatch } = this.props;
-      const { id, names, location, issue } = ticketToEdit;
-      const action = {
-        type: 'ADD_TICKET',
-        id: id,
-        names: names,
-        location: location,
-        issue: issue,
-      }
+      const action = a.addTicket(ticketToEdit);
       dispatch(action);
       this.setState({
         editing: false,
@@ -105,7 +105,7 @@ class TicketControl extends React.Component {
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        <button onClick={this.handleClick}>{buttonText}</button>
+        <button onClick={this.handleClick} className='btn btn-dark'>{buttonText}</button>
       </React.Fragment>
     );
   }
